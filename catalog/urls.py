09f2c71a0f7981e0f1e3d8703053cc7971925bf7
KorsarOfficial <korsarofficial@users.noreply.github.com>
2026-03-13 20:18:11 +0400

    rm legacy sources, keep C++ only

--- catalog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from catalog import views
-
-urlpatterns = [
-    path('home/', views.home_render, name='home'),
-    path('contacts/', views.contacts_render, name='contacts')
-]
--- catalog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home_render(request):
-    return render(request, 'catalog/home.html')
-
-
-def contacts_render(request):
-    return render(request, 'catalog/contacts.html')
